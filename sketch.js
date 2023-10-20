2641f78372b784f2a6eb1ec18a2bd2bd31182d72
ks7150 <144281932+ks7150@users.noreply.github.com>
2023-10-20 22:15:41 +0530

    COVER 2

--- sketch.js
+++ sketch.js
@@ -1,6 +1,74 @@
+let mFont;
+let mSize = 120;
+
+let word = "Quidditch";
+let wordPoints;
+
+
+
+
+let xPos = 0;
+
+
+let yPos = 1000;
+
+
+let xVel = 3;
+
+let cDiam = 15;
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+ 
 }
 
+
+//class Hoop{
+//constructor(){
+  //this.diam = 20
+//this.x= 10
+//this.line(10, 300, 10, 500)
+
+//}
+
+
+
+
 function draw() {
+  noStroke()
+  background("black");
+  fill("gold");
+
+  // draw circle
+  ellipse(random(xPos), random(yPos), cDiam, cDiam);
+
+  // update pos
+  xPos = xPos + xVel;
+
+  // if reset is needed: pick random diameter and reset x
+  if (xPos > width + cDiam) {
+    cDiam = random(15, 10);
+    xPos = 0 - cDiam;
+//if (xPos= mouseX){
+ // xPos=0
+}
+
+
+
+noFill()
+stroke("silver")
+strokeWeight(2)
+ellipse(mouseX, mouseY, 30, 60)
+line(mouseX, mouseY, mouseX, 1000)
+
+
+fill('blue')
+noStroke()
+  redraw([1])
+  textSize(40)
+text('Quidditch', 900, 100, 100, 100)
+
+
 }
